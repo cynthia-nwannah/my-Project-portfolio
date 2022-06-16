c993b5e32bcff71f07c62c2bb905874d586ea9c4
cynthianwannah <chinwendunwannah@yahoo.com>
2022-06-16 17:39:21 +0100

    update my form

--- script.js
+++ script.js
@@ -20,31 +20,17 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   back.classList.remove('active');
 }));
 
-const email = document.querySelector('#email');
-const icon1 = document.querySelector('.icon1');
-const icon2 = document.querySelector('.icon2');
-const error = document.querySelector('.error-message');
-// const btn = document.querySelector('.contact-btn');
+const formbutton = document.getElementById('bot');
+const Email = document.getElementById('email');
+const Errormessage = document.getElementById('error-message');
+Errormessage.textContent = 'Message not sent! Please write using only the small letters. Thank you.';
 
-const regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
-function check() {
-  if (email.value.match(regExp)) {
-    email.style.borderColor = '#27ae60';
-    icon1.style.display = 'none';
-    icon2.style.display = 'block';
-    error.style.display = 'none';
+function validationForm(e) {
+  if (Email.value !== Email.value.toLowerCase()) {
+    Errormessage.style.display = 'block';
+    e.preventDefault();
   } else {
-    email.style.borderColor = '#e7ac3c';
-    icon1.style.display = 'block';
-    icon2.style.display = 'none';
-    error.style.display = 'block';
-  }
-  if (email.value === '') {
-    email.style.borderColor = 'lightgray';
-    icon1.style.display = 'none';
-    icon2.style.display = 'none';
-    error.style.display = 'none';
+    Errormessage.style.display = 'none';
   }
 }
-email.setAttribute('keyup', check());
+formbutton.addEventListener('click', validationForm);
