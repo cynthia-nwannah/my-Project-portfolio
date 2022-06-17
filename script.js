c68e8c4279dd1009a67b392851c54bf516243df3
cynthianwannah <chinwendunwannah@yahoo.com>
2022-06-17 08:20:30 +0100

    remove function

--- script.js
+++ script.js
@@ -24,13 +24,3 @@ const formbutton = document.getElementById('bot');
 const Email = document.getElementById('email');
 const Errormessage = document.getElementById('error-message');
 Errormessage.textContent = 'Message not sent! Please write using only the small letters. Thank you.';
-
-function validationForm(e) {
-  if (Email.value !== Email.value.toLowerCase()) {
-    Errormessage.style.display = 'block';
-    e.preventDefault();
-  } else {
-    Errormessage.style.display = 'none';
-  }
-}
-formbutton.addEventListener('click', validationForm);
